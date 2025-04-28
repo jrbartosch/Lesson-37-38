762cc151145969757e997ae0c87aeda1852b7f2b
jrbartosch <jbartosch+jed@gmail.com>
2025-04-28 08:38:27 +0600

    Add files via upload

--- A1.py
+++ A1.py
@@ -44,10 +44,11 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textx = 10
 texty = 10
 gameoverfont = pygame.font.Font('freesansbold.ttf', 64)
+scoreval = 0
 def show_score(x, y):
-    score = font.render('Score:' + str(score), True, (255, 255, 255))
+    score = font.render('Score:' + str(scoreval), True, (255, 255, 255))
     screen.blit(score, (x, y))
-def game_overtxt():
+def game_over_txt():
     gameovertxt = gameoverfont.render('GAME OVER', True, (255, 255, 255))
     screen.blit(gameovertxt, (200, 250))
 def player(x, y):
@@ -63,11 +64,47 @@ def checkcrash(enemyx, enemyy, bulletx, bullety):
     return distance < crashdist
 running = True
 while running:
+    screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
-    screen.blit(playerimg, (playerx, playery))
-    for i in range(enemynum):
-        screen.blit(enemyimg[i], (enemyx[i], enemyy[i]))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    pygame.display.update()
+           running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerxchange = -5
+            if event.key == pygame.K_RIGHT:
+                playerxchange = 5
+            if event.key == pygame.K_SPACE and bulletstate == 'ready':
+                bulletx = playerx
+                fire_bullet(bulletx, bullety)
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+            playerxchange = 0
+    playerx += playerxchange
+    playerx = max(0, min(playerx, scrnwidth - 64))
+    for i in range(enemynum):
+        if enemyy[i] > 340:
+            for j in range(enemynum):
+                enemyy[j] = 2000
+            game_over_txt()
+            break
+        enemyx[i] += enemyxchange[i]
+        if enemyx[i] <= 0 or enemyx[i] >= scrnwidth - 64:
+            enemyxchange[i] *= -1
+            enemyy[i] += enemyychange[i]
+        if checkcrash(enemyx[i], enemyy[i], bulletx, bullety):
+            bullety = playerstarty
+            bulletstate = 'ready'
+            scoreval += 1
+            enemyx[i] = random.randint(0, scrnwidth - 64)
+            enemyy[i] = random.randint(enemystartymin, enemystartymax)
+        enemy(enemyx[i], enemyy[i], i)
+        if bullety <= 0:
+            bullety = playerstarty
+            bulletstate = 'ready'
+        elif bulletstate == 'fire':
+            fire_bullet(bulletx, bullety)
+            bullety -= bulletychange
+        player(playerx, playery)
+        show_score(textx, texty)
+        pygame.display.update()
+pygame.quit()
